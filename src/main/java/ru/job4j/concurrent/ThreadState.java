94002e16858835fed87c8842dc6dbfd4d426f26a
Sarabik <dalilija@inbox.lv>
2023-02-12 12:40:52 +0100

    3. Состояние нити. [#229175 #390800]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -2,6 +2,7 @@ package ru.job4j.concurrent;
 
 public class ThreadState {
     public static void main(String[] args) {
+
         Thread first = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
