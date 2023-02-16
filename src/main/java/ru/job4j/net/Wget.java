faa773e9240d4f03843f7c78c34598159ba77187
Sarabik <dalilija@inbox.lv>
2023-02-16 19:56:21 +0100

    8. Скачивание файла с ограничением. [#144271 #392140]

--- src/main/java/ru/job4j/net/Wget.java
+++ src/main/java/ru/job4j/net/Wget.java
@@ -9,27 +9,35 @@ import java.net.URL;
 
 public class Wget implements Runnable {
     private final String url;
-    /* speed = n byte per sec */
+    private final String outputFile;
+    /* speed = n Kb per sec */
     private final int speed;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, String outputFile, int speed) {
         this.url = url;
+        this.outputFile = outputFile;
         this.speed = speed;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            int downloadData = 0;
+            long startTime = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                long currentTime = System.currentTimeMillis();
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                long difference = System.currentTimeMillis() - currentTime;
-                if (difference < bytesRead / speed * 1000L) {
-                    Thread.sleep(bytesRead / speed * 1000L - difference);
+                if (downloadData >= speed * 1024) {
+                    long difference = System.currentTimeMillis() - startTime;
+                    if (difference < 1000) {
+                        Thread.sleep(1000 - difference);
+                    }
+                    downloadData = 0;
+                    startTime = System.currentTimeMillis();
                 }
+                downloadData += bytesRead;
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -51,14 +59,15 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        if (args.length != 2) {
+        if (args.length != 3) {
             throw new IllegalArgumentException("Illegal argument count");
         }
         String url = args[0];
         urlValidator(url);
-        int speed = Integer.parseInt(args[1]);
+        String outputFile = args[1];
+        int speed = Integer.parseInt(args[2]);
         speedValidator(speed);
-        Thread wget = new Thread(new Wget(url, speed));
+        Thread wget = new Thread(new Wget(url, outputFile, speed));
         wget.start();
         wget.join();
     }
