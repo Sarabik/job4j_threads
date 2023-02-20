bbf6eff69a2423e4f622433cf41a395cd2e29eb4
Sarabik <dalilija@inbox.lv>
2023-02-20 20:19:54 +0100

    1. Visibility. Общий ресурс вне критической секции [#1102 #392846]

--- src/main/java/ru/job4j/io/ParseFile.java
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -13,10 +13,10 @@ public class ParseFile {
     private synchronized String getContent(Predicate<Character> filter) throws IOException {
         try (BufferedReader i = new BufferedReader(new FileReader(file))) {
             StringBuilder output = new StringBuilder();
-            char data;
-            while ((data = (char) i.read()) > 0) {
-                if (filter.test(data)) {
-                    output.append(data);
+            int data;
+            while ((data = i.read()) != -1) {
+                if (filter.test((char) data)) {
+                    output.append((char) data);
                 }
             }
             return output.toString();
