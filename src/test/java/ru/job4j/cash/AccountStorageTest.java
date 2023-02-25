532d4275ed2cc0b82ed12ae7d2f2e27028c1e75f
Sarabik <dalilija@inbox.lv>
2023-02-25 12:11:25 +0100

    3. Денежные переводы AccountStorage [#1104 #394038]

--- src/test/java/ru/job4j/cash/AccountStorageTest.java
+++ src/test/java/ru/job4j/cash/AccountStorageTest.java
@@ -2,7 +2,6 @@ package ru.job4j.cash;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class AccountStorageTest {
 
