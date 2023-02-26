9499bfa6c62c458d986c4f12cb2dd45f7db69dfd
Sarabik <dalilija@inbox.lv>
2023-02-26 15:33:21 +0100

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/producerconsumer/ParallelSearch.java
@@ -0,0 +1,35 @@
+package ru.job4j.producerconsumer;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        final Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+    }
+}
