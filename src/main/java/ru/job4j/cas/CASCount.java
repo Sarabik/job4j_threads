969bc889357d8e75f584df3a7ce6fcda97724846
Sarabik <dalilija@inbox.lv>
2023-02-28 21:33:47 +0100

    0. CAS - операции [#6859]

---
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -0,0 +1,23 @@
+package ru.job4j.cas;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int temp;
+        int incremented;
+        do {
+            temp = count.get();
+            incremented = temp + 1;
+        } while (!count.compareAndSet(temp, incremented));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
