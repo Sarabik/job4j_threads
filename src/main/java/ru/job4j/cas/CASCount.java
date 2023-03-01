eb0ea9afbfb494e7df449650059603345ae131b1
Sarabik <dalilija@inbox.lv>
2023-03-01 21:45:12 +0100

    0. CAS - операции [#6859 #396262]

--- src/main/java/ru/job4j/cas/CASCount.java
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -6,6 +6,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 @ThreadSafe
 public class CASCount {
+
     private final AtomicReference<Integer> count = new AtomicReference<>(0);
 
     public void increment() {
--- src/test/java/ru/job4j/cas/CASCountTest.java
+++ src/test/java/ru/job4j/cas/CASCountTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CASCountTest {
+
     @Test
     public void whenTestCounterWithTwoThreads() throws InterruptedException {
         final CASCount counter = new CASCount();
