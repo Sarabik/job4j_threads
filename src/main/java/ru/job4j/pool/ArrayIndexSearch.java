b717ecfffaaee2ba0e72beca2747ce3d07ef2d46
Sarabik <dalilija@inbox.lv>
2023-03-05 16:51:17 +0100

    3. ForkJoinPool [#315067]

---
+++ src/main/java/ru/job4j/pool/ArrayIndexSearch.java
@@ -0,0 +1,68 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class ArrayIndexSearch<T> extends RecursiveTask<Integer> {
+    private final T[] array;
+    private final int from;
+    private final int to;
+    private final T element;
+
+    public ArrayIndexSearch(T[] array, int from, int to, T element) {
+        this.array = array;
+        this.from = from;
+        this.to = to;
+        this.element = element;
+    }
+
+    @Override
+    protected Integer compute() {
+        int foundIndex = -1;
+        if (from == to) {
+            if (element.equals(array[from])) {
+                foundIndex = from;
+            }
+        } else {
+            int mid = (from + to) / 2;
+            ArrayIndexSearch<T> left = new ArrayIndexSearch<>(array, from, mid, element);
+            ArrayIndexSearch<T> right = new ArrayIndexSearch<>(array, mid + 1, to, element);
+            left.fork();
+            right.fork();
+            int resultLeft = left.join();
+            int resultRight = right.join();
+            foundIndex = Math.max(resultLeft, resultRight);
+        }
+        return foundIndex;
+    }
+
+    private static <T> int linearSearch(T[] array, T element) {
+        int foundIndex = -1;
+        for (int i = 0; i < array.length; i++) {
+            if (element.equals(array[i])) {
+                foundIndex = i;
+                break;
+            }
+        }
+        return foundIndex;
+    }
+
+    public static <T> int findIndex(T[] array, T element) {
+        if (array.length == 0) {
+            throw new IllegalArgumentException("Array is empty");
+        }
+        int foundIndex;
+        if (array.length <= 10) {
+            foundIndex = linearSearch(array, element);
+        } else {
+            ForkJoinPool forkJoinPool = new ForkJoinPool();
+            foundIndex = forkJoinPool.invoke(new ArrayIndexSearch<>(array, 0, array.length - 1, element));
+        }
+        return foundIndex;
+    }
+
+    public static void main(String[] args) {
+        String[] array = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        System.out.println(findIndex(array, "7"));
+    }
+}
---
+++ src/test/java/ru/job4j/pool/ArrayIndexSearchTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.pool;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static ru.job4j.pool.ArrayIndexSearch.findIndex;
+
+class ArrayIndexSearchTest {
+    @Test
+    public void whenSearchString() {
+        String[] array = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        int index1 = findIndex(array, "7");
+        int index2 = findIndex(array, "12");
+        assertThat(index1).isEqualTo(7);
+        assertThat(index2).isEqualTo(-1);
+    }
+
+    @Test
+    public void whenSearchInteger() {
+        Integer[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int index1 = findIndex(array, 7);
+        int index2 = findIndex(array, 12);
+        assertThat(index1).isEqualTo(7);
+        assertThat(index2).isEqualTo(-1);
+    }
+
+    @Test
+    public void whenArrayLengthIsSmallerThan10ThenLinearSearch() {
+        String[] array = {"0", "1", "2", "3"};
+        int index1 = findIndex(array, "2");
+        int index2 = findIndex(array, "12");
+        assertThat(index1).isEqualTo(2);
+        assertThat(index2).isEqualTo(-1);
+    }
+
+    @Test
+    public void whenArrayIsEmpty() {
+        String[] array = {};
+        assertThatThrownBy(() -> findIndex(array, "7")).isInstanceOf(IllegalArgumentException.class);
+    }
+}
